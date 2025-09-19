291c0b4771c1f417310d9f3092cf39ec18403b13
Brandon Ly <brandonly@lostcoding.com>
2025-09-18 21:01:47 -0500

    rename extension to sleepyduck

--- src/include/sleepyduck_extension.hpp
+++ src/include/sleepyduck_extension.hpp
@@ -4,7 +4,7 @@
 
 namespace duckdb {
 
-class QuackExtension : public Extension {
+class SleepyduckExtension : public Extension {
 public:
 	void Load(ExtensionLoader &db) override;
 	std::string Name() override;
